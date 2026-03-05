c289380c45446c32b85fd5385b16f857423c2ab0
nagipragalathan <nagipragalathan@gmail.com>
2026-03-05 16:24:47 +0530

    Removed direct `gridfs` installation from `build_files.sh` and updated `requirements.txt`.

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 chmod +x build_files.sh
 echo "starting install..."
-python3 -m pip install gridfs --break-system-packages
 python3 -m pip install -r requirements.txt --break-system-packages
 python3 manage.py collectstatic --noinput
 echo "end install-----------------------"
