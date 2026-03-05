814344a863527e39a7502c56e85258207266d5e3
nagipragalathan <nagipragalathan@gmail.com>
2026-03-05 16:06:23 +0530

    chore: Update build script to use `python3` and add `--break-system-packages` to pip install commands.

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chmod +x build_files.sh
 echo "starting install..."
-python3.12 -m pip install gridfs
-python3.12 -m pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput
+python3 -m pip install gridfs --break-system-packages
+python3 -m pip install -r requirements.txt --break-system-packages
+python3 manage.py collectstatic --noinput
 echo "end install-----------------------"
