f5a3f8c36098180313320b4a193ce55826d1f17b
nagipragalathan <nagipragalathan@gmail.com>
2026-03-05 22:38:50 +0530

    feat: Add Acuity Scheduling credential management views (create, edit, list) and custom template filters.

--- base/templatetags/config_tags.py
+++ base/templatetags/config_tags.py
@@ -13,3 +13,7 @@ def filter_by_module(mappings, module_name):
     if not mappings:
         return []
     return [m for m in mappings if m.zoho_module == module_name]
+
+@register.filter
+def split(value, arg):
+    return value.split(arg)
